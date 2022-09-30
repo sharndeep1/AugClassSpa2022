083b41fd1422929d3099989995d6669f7cba72b1
sharndeep1 <sharndeep.chahal@yahoo.in>
2022-09-29 20:10:07 -0500

    adding api

--- components/views/Pizza.js
+++ components/views/Pizza.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 
-export default (state) => html`
-  <section id="pizza">
+export default st => html`
+
     <table id="pizzas">
       <tr>
         <th>Crust</th>
@@ -10,6 +10,15 @@ export default (state) => html`
         <th>Toppings</th>
         <th>Customer</th>
       </tr>
+      ${st.pizzas
+        .map(pizza => {
+          return `<tr><td>${pizza.crust}</td><td>${pizza.cheese}</td><td>${
+            pizza.sauce
+          }</td><td>${pizza.toppings.join(" & ")}</td><td>${
+            pizza.customer
+          }</td></tr>`;
+        })
+        .join("")}
     </table>
-  </section>
+
 `;
